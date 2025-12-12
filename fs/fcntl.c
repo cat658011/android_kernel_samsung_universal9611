159843e01684b7cb4c32fd319c203bdb2e2ecd9a
cat658011 <cat658011@gmail.com>
2025-12-12 19:35:08 +0500

    drop proca

--- fs/fcntl.c
+++ fs/fcntl.c
@@ -429,11 +429,6 @@ static long do_fcntl(int fd, unsigned int cmd, unsigned long arg,
 	case F_FIVE_VERIFY_SYNC:
 		err = five_fcntl_verify_sync(filp);
 		break;
-#if defined(CONFIG_FIVE_PA_FEATURE) || defined(CONFIG_PROCA)
-	case F_FIVE_PA_SETXATTR:
-		err = proca_fcntl_setxattr(filp, (void __user *)arg);
-		break;
-#endif
 	case F_FIVE_EDIT:
 		err = five_fcntl_edit(filp);
 		break;
